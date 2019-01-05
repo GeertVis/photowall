b20f3fc3387530c8caec58f5b64ce7baac299a6c
GeertVis <geertvissers@hotmail.com>
2019-01-05 09:50:39 +0100

    Changes to index.js and package-json files

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {createStore} from 'redux'
 import rootReducer from './redux/reducer'
 import {Provider} from 'react-redux'
 import App from './Components/App'
-import Test:
+
 const store = createStore(rootReducer)
 
 ReactDOM.render(<Provider store={store}><BrowserRouter><App/></BrowserRouter></Provider>, document.getElementById('root'));
